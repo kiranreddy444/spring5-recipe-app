aae59636ab1204b3db9ffe592dcb5f73f971eed7
fatih eren <fatihhernn@gmail.com>
2022-01-13 12:30:16 +0300

    Created Java Pojo class and their relationship

---
+++ src/main/java/fatihhernn/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,42 @@
+package fatihhernn.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uom;
+
+    private String description;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
